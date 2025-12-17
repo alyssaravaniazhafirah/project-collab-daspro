6b2505213bebde47392af6550a791cda5385030b
alyssaravaniazhafirah <alyssaravaniazhafirah@gmail.com>
2025-12-17 22:58:49 +0700

    project uas

---
+++ project collab daspro.py
@@ -0,0 +1,6 @@
+print("              Invoice Tagihan             ")
+
+tanggal_transaksi = input("Masukkan tanggal transaksi: ")
+nomor_invoice = input("Masukkan nomor invoice: ")
+nama_pembeli = input("Masukkan nama pembeli: ")
+alamat_pembeli = input("Masukkan alamat pembeli: ")
