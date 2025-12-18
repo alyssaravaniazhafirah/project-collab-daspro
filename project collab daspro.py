eaefb9467c7ee374d632702fc4a4700104154ec1
raalfiya <alfiyyarahma029@gmail.comm>
2025-12-18 19:15:43 +0700

    update rahma

---
+++ project collab daspro.py
@@ -0,0 +1,17 @@
+print("              Invoice Tagihan             ")
+
+tanggal_transaksi = input("Masukkan tanggal transaksi: ")
+nomor_invoice = input("Masukkan nomor invoice: ")
+nama_pembeli = input("Masukkan nama pembeli: ")
+alamat_pembeli = input("Masukkan alamat pembeli: ")
+
+def garis ():
+    print("_" * 75)
+
+garis()
+print("| No | Deskripsi Produk | Kuantitas | Harga Satuan (Rp) | Total Harga (Rp) |")
+garis()
+print("| 1  | Printer laserjet |     1     |     Rp 800.000    |    Rp 800.000    |")
+garis()
+print("| 2  | Kursi kantor     |     1     |    Rp 1.200.000   |   Rp 1.200.000   |")
+garis()
