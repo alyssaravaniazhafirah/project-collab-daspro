802b7688cd3997d671930b1ad838bf4efcfc2c4d
putri <putrinzr05@gmail.com>
2025-12-18 19:27:05 +0700

    menambahkan data produk

--- project collab daspro.py
+++ project collab daspro.py
@@ -4,3 +4,6 @@ tanggal_transaksi = input("Masukkan tanggal transaksi: ")
 nomor_invoice = input("Masukkan nomor invoice: ")
 nama_pembeli = input("Masukkan nama pembeli: ")
 alamat_pembeli = input("Masukkan alamat pembeli: ")
+# ======================
+# DATA PRODUK
+# ======================
