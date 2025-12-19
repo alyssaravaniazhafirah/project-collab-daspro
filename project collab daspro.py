21470fb5ff1b345be2739d67a1817de66220aecb
alyssaravaniazhafirah <alyssaravaniazhafirah@gmail.com>
2025-12-19 10:42:57 +0700

    update

--- project collab daspro.py
+++ project collab daspro.py
@@ -55,36 +55,3 @@ for i in range(jumlah_barang):
     total_harga_item = hitung_total(kuantitas, harga_satuan) 
     barang.append((nama_barang, kuantitas, harga_satuan, total_harga_item))
     invoice_subtotal += total_harga_item 
-
-# perhitungan
-diskon = hitung_diskon(invoice_subtotal) 
-pajak = hitung_pajak(invoice_subtotal - diskon) 
-total_tagihan = hitung_total_tagihan(invoice_subtotal, diskon, pajak) 
-
-#cetak hasil
-print("\n                          Invoice Tagihan                                ")
-print(f"Tanggal Transaksi : {tanggal_transaksi}")
-print(f"Nomor Invoice     : {nomor_invoice}")
-print(f"Nama Pembeli     : {nama_pembeli}")
-print(f"Alamat Pembeli   : {alamat_pembeli}")
-
-garis()
-print("| No | Deskripsi Produk | Kuantitas | Harga Satuan (Rp) | Total Harga (Rp) |")
-garis()
-for idx, item in enumerate(barang):
-    nama, kuantitas, harga_satuan, total_harga = item
-    print(f"| {idx+1:<2} | {nama:<16} | {kuantitas:^10} | Rp {harga_satuan:>15,} | Rp {total_harga:>15,} |") 
-garis()
-print(f"|Subtotal               : Rp {invoice_subtotal:,.0f}                           |") 
-garis()
-print(f"|Diskon                 : Rp {diskon:,.0f}                                     |")
-garis()
-print(f"|Pajak (5%)             : Rp {pajak:,.0f}                                      |")
-garis()
-print(f"|Total Tagihan          : Rp {total_tagihan:,.0f}                              |")
-garis()
-
-#catatan
-print("Catatan:")
-print("- Diskon 10% berlaku untuk pembelian di atas Rp 2.000.000")
-print("- Pajak 5% dikenakan sesuai peraturan yang berlaku")
